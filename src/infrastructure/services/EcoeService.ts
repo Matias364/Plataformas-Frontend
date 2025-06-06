bc8fbc0a0a4a2401749ab17eeb2936366759e7f2
Matias364 <matias.cortes09@alumnos.ucn.cl>
2025-06-06 11:19:54 -0400

    feat: Add EcoeService for have list ecoes

---
+++ src/infrastructure/services/EcoeService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export interface Ecoe{
+    id: number;
+    semestre: number;
+    description: string;
+}
+
+export const getAvailableEcoes = async (): Promise<Ecoe[]> => {
+    try {
+        const response = await axios.get<Ecoe[]>('http://localhost:3002/api/v1/ecoes/available');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching ecoes:', error);
+        throw error;
+    }
+}
