0223a1891a6a9f4269631cb7756d0458a37fa27f
pwn922 <mauricio41533@gmail.com>
2025-06-20 23:02:18 -0400

    refactor(EcoeService): improve interface structure and error handling

--- src/infrastructure/services/EcoeService.ts
+++ src/infrastructure/services/EcoeService.ts
@@ -1,17 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
-export interface Ecoe{
+export interface Ecoe {
     id: number;
+    name: string;
+    cycle: 'BASICO' | 'PROFESIONAL' | 'FINAL';
+    year: number;
     semester: number;
     description: string;
 }
 
-export const getAvailableEcoes = async (): Promise<Ecoe[]> => {
+export interface Student {
+    rut: string;
+    name: string;
+    email: string;
+    grade: number;
+}
+
+const API_URL = import.meta.env.VITE_BACKEND_ECOE_URL;
+
+export const getAvailableEcoes = async (cycle: string): Promise<Ecoe[]> => {
     try {
-        const response = await axios.get<Ecoe[]>('http://localhost:3002/api/v1/ecoes/available');
+        const response = await axios.get<Ecoe[]>(`${API_URL}/ecoes/by-cycle/${cycle}`);
         return response.data;
     } catch (error) {
-        console.error('Error fetching ecoes:', error);
-        throw error;
+        console.error("Error al obtener ECOEs", error);
+        throw new Error("No se pudieron obtener los ECOEs");
     }
-}
+};
+
+export const fetchStudentsByEcoeId = async (ecoeId: number): Promise<Student[]> => {
+    try {
+        const response = await axios.get<Student[]>(`${API_URL}/ecoes/${ecoeId}/students`);
+        return response.data;
+    } catch (error) {
+        console.error("Error al obtener estudiantes", error);
+        throw new Error("No se pudieron obtener los estudiantes del ECOE");
+    }
+};
