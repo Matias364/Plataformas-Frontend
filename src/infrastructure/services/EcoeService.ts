6cc61c1644cc299ac31d7f388d236ecc4d182137
Matias364 <matias.cortes09@alumnos.ucn.cl>
2025-06-21 20:13:26 -0400

    update(EcoeService): Methods for handling student data were added

--- src/infrastructure/services/EcoeService.ts
+++ src/infrastructure/services/EcoeService.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
-
-export interface Ecoe{
-    id: number;
-    semester: number;
-    description: string;
-}
+import { Ecoe } from '../../domain/ecoe/Ecoe';
+import { EcoeYear } from '../../domain/ecoe/EcoeYear';
 
 export const getAvailableEcoes = async (): Promise<Ecoe[]> => {
     try {
@@ -14,4 +10,22 @@ export const getAvailableEcoes = async (): Promise<Ecoe[]> => {
         console.error('Error fetching ecoes:', error);
         throw error;
     }
-}
+};
+
+export const getStudentEcoeYears = async (userId: string): Promise<EcoeYear[]> => {
+    const response = await axios.get<EcoeYear[]>(`http://localhost:3001/api/v1/students/${userId}/ecoeYears`);
+    if (response.status !== 200) {
+        throw new Error('Error fetching ecoe years');
+    }
+    return response.data;
+};
+
+export const getStudentEcoeCompetencies = async (userId: string, ecoeId: number) => {
+    const response = await axios.get(`http://localhost:3001/api/v1/students/${userId}/ecoe/${ecoeId}`);
+    return response.data;
+};
+
+export const getStudentEcoeAvg = async (userId: string, ecoeId: number) => {
+    const response = await axios.get(`http://localhost:3001/api/v1/students/${userId}/ecoe/${ecoeId}/avg`);
+    return response.data;
+};
