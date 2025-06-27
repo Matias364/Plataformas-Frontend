107ff62be8aa47c068ee125bce9038e0130c17b4
Miguel Galleguillos <miguel.galleguillos01@alumnos.ucn.cl>
2025-06-26 23:09:19 -0400

    fix(constants): update API endpoints to remove versioning

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 
-export const LOGIN_ENDPOINT="/api/v1/auth/login-gmail"
+export const LOGIN_ENDPOINT="/auth/login-gmail"
 export const LOGIN_URL= import.meta.env.VITE_BACKEND_URL + LOGIN_ENDPOINT
-export const USER_INFO_ENDPOINT = "/api/v1/auth/users/me"
+export const USER_INFO_ENDPOINT = "/auth/users/me"
 export const USER_INFO_URL = import.meta.env.VITE_BACKEND_URL + USER_INFO_ENDPOINT
-export const LOGIN_ADMIN_ENDPOINT="/api/v1/auth/login-local"
+export const LOGIN_ADMIN_ENDPOINT="/auth/login-local"
 export const LOGIN_ADMIN_URL= import.meta.env.VITE_BACKEND_URL + LOGIN_ADMIN_ENDPOINT
 
 
