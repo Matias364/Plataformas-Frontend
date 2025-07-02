f51284d43840fe2184b17424fd4a2ecd5c5771b1
Miguel Galleguillos <miguel.galleguillos01@alumnos.ucn.cl>
2025-07-02 00:43:20 -0400

    refactor(ICourseService): update getSubjectTeacher method to return Subject array instead of Course array

--- src/application/course/ICourseService.ts
+++ src/application/course/ICourseService.ts
@@ -1,6 +1,7 @@
 import { Course } from '../../domain/course/Course';
+import { Subject } from '../../domain/course/Subject';
 
 export interface ICourseService {
-  getSubjectTeacher(): Promise<Course[]>;
+  getSubjectTeacher(): Promise<Subject[]>;
   getTeacherCourses(): Promise<Course[]>; // New method for /courses/teacher
 }
