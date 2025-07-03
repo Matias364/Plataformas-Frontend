05b78c2e7817738e619b9898151fd5e0e43a78c8
Miguel Galleguillos <miguel.galleguillos01@alumnos.ucn.cl>
2025-07-03 12:05:53 -0400

    feat(ProgramDirectorIndex): create main entry component for Program Director

---
+++ src/presentation/pages/ProgramDirector/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProgramDirectorMainLayout from './ProgramDirectorMainLayout';
+
+/**
+ * Componente principal para el Director de Programa
+ * Este componente sirve como punto de entrada para todas las funcionalidades
+ * del director de programa
+ */
+const ProgramDirectorIndex: React.FC = () => {
+  return <ProgramDirectorMainLayout />;
+};
+
+export default ProgramDirectorIndex;
